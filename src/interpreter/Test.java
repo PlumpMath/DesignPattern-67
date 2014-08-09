67e29be95af5be1cbb5cf9a1ba489bfbdbf85f91
hailong <304381616@qq.com>
2014-08-09 10:19:17 +0800

    Interpreter

---
+++ src/interpreter/Test.java
@@ -0,0 +1,16 @@
+package interpreter;
+
+/**
+ * 解释器模式（Interpreter）
+ * 
+ * @author HL
+ */
+public class Test {
+	public static void main(String[] args) {
+
+		// 计算9+2-8的值
+		int result = new Minus().interpret((new Context(new Plus()
+				.interpret(new Context(9, 2)), 8)));
+		System.out.println(result);
+	}
+}
--- src/mediator/Test.java
+++ src/mediator/Test.java
@@ -1,5 +1,10 @@
 package mediator;
 
+/**
+ * 中介者模式（Mediator）
+ * 
+ * @author HL
+ */
 public class Test {
 
 	public static void main(String[] args) {
