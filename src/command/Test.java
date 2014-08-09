bb32f210cd2f373f8e1792fabccb7b3723a96ce1
hailong <304381616@qq.com>
2014-08-09 08:49:10 +0800

    command

---
+++ src/command/Test.java
@@ -0,0 +1,17 @@
+package command;
+
+/**
+ * 命令模式（Command）
+ * 
+ * @author HL
+ * 
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		Receiver receiver = new Receiver();
+		Command cmd = new MyCommand(receiver);
+		Invoker invoker = new Invoker(cmd);
+		invoker.action();
+	}
+}
