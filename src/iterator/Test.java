9fa59429f1f89e6b9132a0a5c97199642713385a
hailong <304381616@qq.com>
2014-08-09 08:30:51 +0800

    iterator

---
+++ src/iterator/Test.java
@@ -0,0 +1,18 @@
+package iterator;
+
+/**
+ * 迭代子模式（Iterator）
+ * 
+ * @author HL
+ * 
+ */
+public class Test {
+	public static void main(String[] args) {
+		Collection collection = new MyCollection();
+		Iterator it = collection.iterator();
+
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
+}
