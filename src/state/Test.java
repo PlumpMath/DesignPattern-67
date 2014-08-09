185f1a3ff4f5f44555dfd61b7ec3f810b6239d44
hailong <304381616@qq.com>
2014-08-09 09:59:41 +0800

    state

---
+++ src/state/Test.java
@@ -0,0 +1,22 @@
+package state;
+
+/**
+ * 状态模式（State）
+ * 
+ * @author HL
+ */
+public class Test {
+	public static void main(String[] args) {
+
+		State state = new State();
+		Context context = new Context(state);
+
+		// 设置第一种状态
+		state.setValue("state1");
+		context.method();
+
+		// 设置第二种状态
+		state.setValue("state2");
+		context.method();
+	}
+}
